d7ee27adec9439a3f664aa2a63fa20d1651a196d
Shunjid Rahman Showrov <shunjid1862@gmail.com>
2018-09-01 23:27:42 +0600

    Create ProjectStructure.py

---
+++ ProjectFile/ProjectStructure.py
@@ -0,0 +1,70 @@
+def tree(dir, padding, print_files=False):
+    print padding[:-1] + '+-' + basename(abspath(dir)) + '/'
+    padding = padding + ' '
+    files = []
+    if print_files:
+        files = listdir(dir)
+    else:
+        files = [x for x in listdir(dir) if isdir(dir + sep + x)]
+    count = 0
+    for file in files:
+        count += 1
+        print padding + '|'
+        path = dir + sep + file
+        if isdir(path):
+            if count == len(files):
+                tree(path, padding + ' ', print_files)
+            else:
+                tree(path, padding + '|', print_files)
+        else:
+            print padding + '+-' + file
+
+def usage():
+    return '''Usage: %s [-f] <PATH>
+Print tree structure of path specified.
+Options:
+-f      Print files as well as directories
+PATH    Path to process''' % basename(argv[0])
+
+def main():
+print ("├───build
+│   ├───empty
+│   ├───generated-sources
+│   │   └───ap-source-output
+│   └───web
+│       ├───assets
+│       │   ├───css
+│       │   ├───demo
+│       │   ├───img
+│       │   ├───js
+│       │   │   ├───core
+│       │   │   └───plugins
+│       │   └───scss
+│       ├───META-INF
+│       └───WEB-INF
+│           ├───classes
+│           │   └───com
+│           │       └───FilesPack
+│           └───lib
+├───dist
+├───lib
+│   ├───CopyLibs
+│   └───javaee-endorsed-api-7.0
+├───nbproject
+│   └───private
+├───src
+│   ├───conf
+│   └───java
+│       └───com
+│           └───FilesPack
+└───web
+    ├───assets
+    │   ├───css
+    │   ├───demo
+    │   ├───img
+    │   ├───js
+    │   │   ├───core
+    │   │   └───plugins
+    │   └───scss
+    ├───META-INF
+    └───WEB-INF")
